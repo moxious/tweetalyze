6d8dc5a6d2d084fa20765e0978d65b0af8d7ec4e
Moxious <moxious@oldhat.org>
2018-02-17 10:24:50 -0500

    update

--- sipper/index.js
+++ sipper/index.js
@@ -12,24 +12,33 @@ const creds = require('./creds.json');
 
 let c = 0;
 
+const insertTweet = tweet => {
+  const findCriteria = _.pick(tweet, ["id_str"]);
+
+  // Depends on id_str index.
+  collection.updateOne(findCriteria, { $set: tweet }, { upsert: true })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.error('Upsert failed: ', err));
+
+  // collection.insert([tweet], (err, result) => {
+  //   if (err) { console.error(err); }
+  //   else if (result.result.n !== 1) { console.error('Unexpected result ', result); }
+  //   else {
+  //     c++;
+  //     if (c % 1000 === 0) { console.log('Checkpoint ', c); }
+  //   }
+  // });
+};
+
 const beginCapture = () => {
   T = new Twit(_.merge(creds, {
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
   }));
 
   var stream = T.stream('statuses/filter', { track: 'Russians, #politics, #trumptrain, #MAGA, #Mueller, Kremlin, Putin', language: 'en' })
-
-  stream.on('tweet', function (tweet) {
-    // console.log(tweet.user.screen_name + ': ' + tweet.text); 
-    collection.insert([tweet], (err, result) => {
-      if (err) { console.error(err); }
-      else if (result.result.n !== 1) { console.error('Unexpected result ', result); }
-      else {
-        c++;
-        if (c % 1000 === 0) { console.log('Checkpoint ', c); }
-      }
-    });
-  });
+  stream.on('tweet', insertTweet);
 };
 
 MongoClient.connect(url, (err, client) => {
